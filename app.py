a36322e3554d0e203dfd5e6799848f3108a70209
Balogun Hammed <hammedbalogun@gmail.com>
2026-05-13 05:39:33 -0700

    Updated

--- app.py
+++ app.py
@@ -26,8 +26,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DB_URI")
 app.config['MAIL_SERVER'] = "smtp.gmail.com"
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = "hammedbalogun740@gmail.com"
-app.config['MAIL_PASSWORD'] = "hocipyptjqadklny"
+app.config['MAIL_USERNAME'] = os.getenv("DEFAULT_EMAIL")
+app.config['MAIL_PASSWORD'] = os.getenv("EMAIL_PASSWORD")
 app.config['MAIL_DEFAULT_SENDER'] = "hammedbalogun740@gmail.com"
 
 mail = Mail(app)
